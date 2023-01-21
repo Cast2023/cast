287c58c64f158f742e174544c1c76dec37f432e8
Peksu <75335748+Pekk4@users.noreply.github.com>
2023-01-21 21:12:29 +0200

    Make some fine tuning

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import sys
 
 
-if __name__ == "__main__":
-    with open('.env', 'w') as f:
-        for i in sys.argv[1].split(";"):
-            f.write("%s\n" % i)
+with open('.testenv', 'w') as f:
+    for i in sys.argv[1].split(";"):
+        f.write("%s\n" % i)
