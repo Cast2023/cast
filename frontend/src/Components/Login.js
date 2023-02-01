679b6e90e9a7cc9a8effbfbb06f9ca07282d4774
JY Fang <junyuan.fang@helsinki.fi>
2023-02-01 09:03:38 +0200

    login handled with the router

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,14 +1,37 @@
+import { Button, Grid } from "@mui/material"
 import "./Login.css"
 
-const Login = () => (
-  <div className="login">
-    <div className="rectangle-1">
-      <span className="cast">CAST</span>
-      <button className="rectangle-2">
-        <span className="googlelogin">Googlelogin</span>
-      </button>
-    </div>
-  </div>
+// const Login = ({setSessionState}) => (
+//   <div className="login">
+//     <div className="rectangle-1">
+//       <span className="cast">CAST</span>
+//       <button className="rectangle-2">
+//         <span className="googlelogin">Googlelogin</span>
+//       </button>
+//     </div>
+//   </div>
+// )
+
+// const handleLogIn=({event, setSessionState})=>{
+//     event.preventDefault()
+//     setSessionState(true)
+// }
+
+const Login = ({handleLogIn}) => (
+  <Grid container>
+    <form onSubmit={handleLogIn}>
+      <Grid item alignItems="stretch" style={{ display: "flex" }}>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          value="Submit"
+        >
+          Log in
+        </Button>
+      </Grid>
+    </form>
+  </Grid>
 )
 
 export default Login
