57e4498d0ac6506b1ee390b80888c2df435c2b1f
Reetta S <reetta.savaloja@helsinki.fi>
2023-02-03 13:29:18 +0200

    popup button

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,16 +1,53 @@
-const Home = () => (
-  <div>
-    <h2>Welcome to CAS-tracker</h2>
+import { Button, Popover, Typography } from "@mui/material";
+import { useState } from "react";
 
+const Home = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  return(
+    <div>
+    <h2>Welcome to CAS-tracker</h2>
     <br />
     <div>
       Recent news/updates:
       <br />
       <br />
-      TÄHÄN NAPPI
+      <br />
+      <div>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={(event) => {
+          setAnchorEl(event.currentTarget);
+        }}
+      >
+        Get Started
+      </Button>
+      <Popover
+        anchorEl={anchorEl}
+        open={open}
+        id={open ? "simple-popover" : undefined}
+        onClose={() => {
+          setAnchorEl(null);
+        }}
+        transformOrigin={{
+          horizontal: "center",
+          vertical: "top",
+        }}
+        anchorOrigin={{
+          horizontal: "center",
+          vertical: "bottom",
+        }}
+      >
+        This is very helpful
+        </Popover>
+    </div>
       <br /> 
       </div>
     </div>
-)
+  )
+
+}
 
 export default Home
