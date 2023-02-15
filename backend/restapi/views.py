7c3c3a460fd0e22ab64688cb870f8197a7162d3a
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-02-15 10:10:43 +0200

    First fully working version of API

--- backend/restapi/views.py
+++ backend/restapi/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
 
 from restapi.models import Employee_tech_skills, Employees
 from .serializers import TechSkillSerializer, ConsultantSerializer
@@ -7,27 +8,35 @@ from .serializers import TechSkillSerializer, ConsultantSerializer
 class TechAPIView(viewsets.ModelViewSet):
     serializer_class = TechSkillSerializer
     queryset = Employee_tech_skills.objects.all()
-   
-# class ConsultantAPIView(viewsets.ModelViewSet):
-#     """
-#     This viewset automatically provides `list`, `create`, `retrieve`,
-#     `update` and `destroy` actions.
-
-#     Additionally we also provide an extra `highlight` action.
-#     """
-#     queryset = Employees.objects.all()
-#     serializer_class = ConsultantSerializer
     
 
 class ConsultantAPIView(viewsets.ModelViewSet):
     queryset = Employees.objects.all()
     serializer_class = ConsultantSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    filterset_fields = ['id', 'first_name', 'last_name', 'skills']
-    search_fields = ['first_name', 'last_name']
-
-
-
-
+    filterset_fields = [
+        'id',
+        'first_name',
+        'last_name', 
+        'location_city', 
+        'location_country', 
+        'email', 
+        'phone_number', 
+        'worktime_allocation', 
+        'wants_to_do', 
+        'wants_not_to_do'
+        ]
+    search_fields = [
+        'first_name', 
+        'last_name', 
+        'location_city', 
+        'location_country', 
+        'email', 
+        'phone_number', 
+        'worktime_allocation', 
+        'wants_to_do', 
+        'wants_not_to_do', 
+        'techs__tech_name'
+        ]
 
 
