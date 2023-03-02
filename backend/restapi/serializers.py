928ff598fc3bbbb9d8260e9d1b14d1bbac01b13d
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-03-02 19:01:29 +0200

    Add parsing of certificate - information

--- backend/restapi/serializers.py
+++ backend/restapi/serializers.py
@@ -10,49 +10,65 @@ class TechSerializer(serializers.ModelSerializer):
         model = Techs
         fields = ('id', 'tech_name')
 
+
 class CertSerializer(serializers.ModelSerializer):
     class Meta:
         model = Certificates
         fields = ('id', 'certificate_name')
 
+
 class TechSkillSerializer(serializers.ModelSerializer):
     tech_name = serializers.StringRelatedField(source='tech.tech_name')
+
     class Meta:
         model = Employee_tech_skills
         fields = ('skill_level', 'tech', 'tech_name')
 
+
 class EmployeeCertSerializer(serializers.ModelSerializer):
-    cert_name = serializers.StringRelatedField(source='certificate.certificate_name')
+    cert_name = serializers.StringRelatedField(
+        source='certificate.certificate_name')
 
     class Meta:
         model = Employee_certificates
         fields = ('certificate_name, valid_until')
 
+
 class ConsultantSerializer(serializers.ModelSerializer):
     skills = TechSkillSerializer(many=True)
     certs = EmployeeCertSerializer(many=True)
+
     class Meta:
         model = Employees
-        # fields = ('first_name', 'last_name', 'email', 'skills')
+        # fields = ('first_name', 'last_name,', 'email', 'skills')
         fields = ('__all__')
         depth = 2
-    
+
     def update(self, instance, validated_data):
         '''
             TBD: change changes to all employee-fields. 
             Con
         '''
-           
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
+
+        instance.first_name = validated_data.get(
+            'first_name', instance.first_name)
+        instance.last_name = validated_data.get(
+            'last_name', instance.last_name)
         instance.email = validated_data.get('email', instance.email)
-        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
-        instance.location_country = validated_data.get('location_country', instance.location_country)
-        instance.location_city = validated_data.get('location_city', instance.location_city)
-        instance.worktime_allocation = validated_data.get('worktime_allocation', instance.worktime_allocation)
-        instance.allocation_until = validated_data.get('allocation_until', instance.allocation_until)
-        instance.wants_to_do = validated_data.get('wants_to_do', instance.wants_to_do)
-        instance.wants_not_to_do = validated_data.get('wants_not_to_do', instance.wants_not_to_do)
+        instance.phone_number = validated_data.get(
+            'phone_number', instance.phone_number)
+        instance.location_country = validated_data.get(
+            'location_country', instance.location_country)
+        instance.location_city = validated_data.get(
+            'location_city', instance.location_city)
+        instance.worktime_allocation = validated_data.get(
+            'worktime_allocation', instance.worktime_allocation)
+        instance.allocation_until = validated_data.get(
+            'allocation_until', instance.allocation_until)
+        instance.wants_to_do = validated_data.get(
+            'wants_to_do', instance.wants_to_do)
+        instance.wants_not_to_do = validated_data.get(
+            'wants_not_to_do', instance.wants_not_to_do)
         instance.save()
 
         if 'skills' in validated_data:
@@ -62,13 +78,15 @@ class ConsultantSerializer(serializers.ModelSerializer):
             for updated_skill in updated_skill_list:
                 updated = False
                 for skill in consultant_skills:
-                    if updated_skill['tech']== skill.tech:
+                    if updated_skill['tech'] == skill.tech:
                         skill.skill_level = updated_skill['skill_level']
                         skill.save()
                         updated = True
                 if not updated:
-                    Employee_tech_skills.objects.create(employee=instance, tech=updated_skill['tech'], skill_level=updated_skill['skill_level'])
+                    Employee_tech_skills.objects.create(
+                        employee=instance, tech=updated_skill['tech'], skill_level=updated_skill['skill_level'])
         return instance
-    
+
+
 class FileUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
--- backend/restapi/views.py
+++ backend/restapi/views.py
@@ -1,27 +1,35 @@
 from rest_framework import viewsets, filters, generics, status
-from django_filters import rest_framework as rest_filters 
-import io, csv, pandas as pd
+from django_filters import rest_framework as rest_filters
+import io
+import csv
+import pandas as pd
 from rest_framework.response import Response
 
 
 from restapi.models import Employees, Techs, Certificates
 from .serializers import TechSerializer, CertSerializer, ConsultantSerializer, FileUploadSerializer
 
+
 class TechAPIView(viewsets.ModelViewSet):
     serializer_class = TechSerializer
     queryset = Techs.objects.all()
+
 
 class CertAPIView(viewsets.ModelViewSet):
     serializer_class = CertSerializer
     queryset = Certificates.objects.all()
 
+
 class EmployeeFilter(rest_filters.FilterSet):
-    first_name = rest_filters.CharFilter(field_name='first_name', lookup_expr='icontains')
-    last_name = rest_filters.CharFilter(field_name='last_name', lookup_expr='icontains')
+    first_name = rest_filters.CharFilter(
+        field_name='first_name', lookup_expr='icontains')
+    last_name = rest_filters.CharFilter(
+        field_name='last_name', lookup_expr='icontains')
 
     class Meta:
         fields = ("first_name",)
         model = Employees
+
 
 class ConsultantAPIView(viewsets.ModelViewSet):
     queryset = Employees.objects.all()
@@ -29,33 +37,35 @@ class ConsultantAPIView(viewsets.ModelViewSet):
     filter_backends = [rest_filters.DjangoFilterBackend, filters.SearchFilter]
     filterset_class = EmployeeFilter
     search_fields = [
-        'first_name', 
-        'last_name', 
-        'location_city', 
-        'location_country', 
-        'email', 
-        'phone_number', 
-        'worktime_allocation', 
-        'wants_to_do', 
-        'wants_not_to_do', 
+        'first_name',
+        'last_name',
+        'location_city',
+        'location_country',
+        'email',
+        'phone_number',
+        'worktime_allocation',
+        'wants_to_do',
+        'wants_not_to_do',
         'techs__tech_name'
-        ]
+    ]
+
 
 class ImportCertificatesView(generics.CreateAPIView):
     serializer_class = FileUploadSerializer
-    
+
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         file = serializer.validated_data['file']
-        reader = pd.read_csv(file)
-        print(reader)
-        # for _, row in reader.iterrows():
-        #     if Employees.objects.get(email=row['Name']):
-        #         new_certificate = Employees(
-        #                 Certificates(
-        #                     valid_until = ["Expiration Date (DD/MM/YYYY)"]
-        #                 )
-        #                 )
-        #         new_certificate.save()
+        content = pd.read_csv(file)
+        data_as_dict = {}
+        for index, row in content.iterrows():
+            name = row['Name']
+            certificate = [col for col in content.columns[1:]
+                           if str(row[col]).lower() == 'x']
+            certificate.append(row["Expiration Date (DD/MM/YYYY)"])
+            if not name in data_as_dict:
+                data_as_dict[name] = []
+            data_as_dict[name].append(certificate)
+        print(data_as_dict)
         return Response({"status": "success"}, status.HTTP_201_CREATED)
