4913731e5eaa16938b2c7154a60680e94891a2e2
Reetta S <reetta.savaloja@helsinki.fi>
2023-03-15 21:57:15 +0200

    git work. Reetta happy

--- backend/cast_backend/settings.py
+++ backend/cast_backend/settings.py
@@ -16,7 +16,7 @@ import json
 from dotenv import dotenv_values
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'. does branch work 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV = dotenv_values(Path(BASE_DIR).joinpath(".env"))
 
