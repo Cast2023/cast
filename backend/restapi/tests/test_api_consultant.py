036addef37b099aa098e1f4cce0c0d2507c8aa3a
Maija <maija.saisa@helsinki.fi>
2023-03-20 09:04:13 +0200

    Expand API tests

--- backend/restapi/tests/test_api_consultant.py
+++ backend/restapi/tests/test_api_consultant.py
@@ -2,16 +2,23 @@ from django.test import Client
 from django.test.client import encode_multipart
 from django.urls import reverse
 from rest_framework.test import APIRequestFactory, APITestCase, APIClient
-from restapi.models import Employees, Techs, Employee_tech_skills
-
+from restapi.models import (
+    Employees,
+    Techs,
+    Employee_tech_skills,
+    Certificate,
+    Employee_certificates,
+    Project,
+    Employee_projects
+)
 
 class EmployeeGetTests(APITestCase):
     url = reverse('consultant-list')
     
     def setUp(self):
-        self.client = Client()
+        client = Client()
 
-        Employees.objects.create(
+        user1 = Employees.objects.create(
             first_name='John',
             last_name='Doe',
             email='tester@gmail.com'
@@ -28,31 +35,85 @@ class EmployeeGetTests(APITestCase):
             wants_to_do='Manufacturing sector',
             wants_not_to_do='Banking sector'
         )
+        tech1 = Techs.objects.create(
+            tech_name='Python'
+        )
+        cert1 = Certificate.objects.create(
+            vendor='Amazon',
+            certificate_name='AWS Certified Cloud Practitioner'
+        )
+        project1 = Project.objects.create(
+            project_name='CastCorp',
+            project_start_date='2021-10-19',
+            project_end_date='2022-05-15',
+            confidential=False
+        )
+        Employee_tech_skills.objects.create(
+            employee = user1,
+            tech = tech1,
+            skill_level = 3
+        )  
+        Employee_certificates.objects.create(
+            employee = user1,
+            cert = cert1,
+            valid_until = '2025-01-01'
+        )
+        Employee_projects.objects.create(
+            employee = user1,
+            project = project1,
+            employee_participation_start_date = '2022-01-01',
+            employee_participation_end_date = '2022-03-30',
+            allocation_busy = 60,
+        )
+        self.response = client.get(self.url)
+        self.result = self.response.json()
 
     def test_get_employees_returns_status_code_ok(self):
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
-    def test_get_employees_returns_a_list(self):
-        response = self.client.get(self.url)
-        result = response.json()
-        self.assertIsInstance(result, list)
+    def test_get_employees_returns_a_list(self):        
+        self.assertIsInstance(self.result, list)
     
-    def test_get_employees_returns_a_created_user(self):
-        response = self.client.get(self.url)
-        result = response.json()
-        self.assertEqual(result[1]['first_name'], 'Jane')
-        self.assertEqual(result[1]['last_name'], 'Watson')
-        self.assertEqual(result[1]['email'], 'watson@gmail.com')
-        self.assertEqual(result[1]['phone_number'], '+358502345678')
-        self.assertEqual(result[1]['location_country'], 'Finland')
-        self.assertEqual(result[1]['location_city'], 'Helsinki')
-        self.assertEqual(result[1]['worktime_allocation'], 100)
-        self.assertEqual(result[1]['allocation_until'], '2029-12-31')
-        self.assertEqual(result[1]['wants_to_do'], 'Manufacturing sector')
-        self.assertEqual(result[1]['wants_not_to_do'], 'Banking sector')
+    def test_get_employees_returns_correct_name_for_single_user(self):
+        self.assertEqual(self.result[1]['first_name'], 'Jane')
+        self.assertEqual(self.result[1]['last_name'], 'Watson')
+        
+    def test_get_employees_returns_correct_contact_info_for_single_user(self):
+        self.assertEqual(self.result[1]['email'], 'watson@gmail.com')
+        self.assertEqual(self.result[1]['phone_number'], '+358502345678')
+        
+    def test_get_employees_returns_correct_location_for_single_user(self):
+        self.assertEqual(self.result[1]['location_country'], 'Finland')
+        self.assertEqual(self.result[1]['location_city'], 'Helsinki')
+        
+    def test_get_employees_returns_correct_worktime_info_for_single_user(self):
+        self.assertEqual(self.result[1]['worktime_allocation'], 100)
+        self.assertEqual(self.result[1]['allocation_until'], '2029-12-31')
+    
+    def test_get_employees_returns_correct_preferences_for_single_user(self):
+        self.assertEqual(self.result[1]['wants_to_do'], 'Manufacturing sector')
+        self.assertEqual(self.result[1]['wants_not_to_do'], 'Banking sector')
+        
+    def test_get_employees_returns_correct_certs_for_single_user(self):
+        certs = self.result[0]['certificates']
+        self.assertEqual(len(certs), 1)
+        self.assertEqual(certs[0]['vendor'], 'Amazon')
+        self.assertEqual(certs[0]['certificate'], 'AWS Certified Cloud Practitioner')
+        self.assertEqual(certs[0]['valid_until'], '2025-01-01')
+        
+    def test_get_employees_returns_correct_projects_for_single_user(self):
+        projects = self.result[0]['projects']
+        self.assertEqual(len(projects), 1)
+        self.assertEqual(projects[0]['project_name'], 'CastCorp')
+        self.assertEqual(projects[0]['project_start_date'], '2021-10-19')
+        self.assertEqual(projects[0]['employee_participation_start_date'], '2022-01-01')
+        self.assertEqual(projects[0]['employee_participation_end_date'], '2022-03-30')
+        self.assertEqual(projects[0]['project_end_date'], '2022-05-15')
+        self.assertEqual(projects[0]['allocation_busy'], 60)
+        self.assertEqual(projects[0]['confidential'], "False")
 
 
+        
 class EmployeeUpdateTests(APITestCase):
     """Each TestCase is a transaction and so there is no pre-knowledge of the id's of database instances.
         Because of this the created employees are first fetched and an id is extracted to ensure that
