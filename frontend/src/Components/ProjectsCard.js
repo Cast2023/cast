63b13891fa6dc021c6908890422068708de5113d
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-03-22 17:56:39 +0200

    Initial expansion when add is clicked

--- frontend/src/Components/ProjectsCard.js
+++ frontend/src/Components/ProjectsCard.js
@@ -1,29 +1,79 @@
-import { 
+import {
   Card,
-  CardHeader, 
-  CardContent, 
+  CardHeader,
+  CardContent,
   IconButton,
   Box,
+  TextField,
 } from "@mui/material"
-  
-import { DataGrid } from '@mui/x-data-grid'
-import EditIcon from '@mui/icons-material/Edit'
-  
-  
+import { useSelector, useDispatch } from "react-redux"
+import AddCircleIcon from "@mui/icons-material/AddCircle"
+import EditIcon from "@mui/icons-material/Edit"
+import { updateAddState } from "../Reducers/projectCardReducer"
+import Autocomplete from "@mui/material/Autocomplete"
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import { DatePicker } from "@mui/x-date-pickers/DatePicker"
+
 const ProjectsCard = ({ user, activeUserId }) => {
+  const dispatch = useDispatch()
+  const addProjectState = useSelector(
+    (state) => state.projectCard.addProjectActivated
+  )
+
+  const projects = useSelector((state) => state.projectCard.allProjects)
+
+  const updateAddProjectState = (addProjectState) => {
+    dispatch(updateAddState(!addProjectState))
+  }
+
   return (
     <div>
       <Card>
         <CardHeader
           title="Projects"
-          action={(user.id === activeUserId) && (
-            <IconButton id="editProjectsButton">
-              <EditIcon />
-            </IconButton>
-          )}
-          />
-        <CardContent> 
-        
+          action={
+            user.id === activeUserId && (
+              <Box>
+                <IconButton
+                  id="add_skills_button"
+                  onClick={() => updateAddProjectState(addProjectState)}
+                >
+                  <AddCircleIcon />
+                </IconButton>
+                <IconButton id="editProjectsButton">
+                  <EditIcon />
+                </IconButton>
+              </Box>
+            )
+          }
+        />
+        <CardContent>
+          <Box>
+            {addProjectState && (
+              <Box>
+                <Autocomplete
+                  disablePortal
+                  id="projects-combo-box"
+                  options={projects.map((project) => project.project_name)}
+                  sx={{ width: 300 }}
+                  renderInput={(params) => (
+                    <TextField {...params} project_name="project" />
+                  )}
+                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker />
+                </LocalizationProvider>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker />
+                </LocalizationProvider>
+                <TextField
+                  type="number"
+                  inputProps={{ min: 0, max: 100, step: "5" }}
+                ></TextField>
+              </Box>
+            )}
+          </Box>
         </CardContent>
       </Card>
     </div>
