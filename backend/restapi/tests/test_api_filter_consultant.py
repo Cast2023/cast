37a9cc42aff9178fc772f71a6a4b1f22569eb963
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-03-29 11:55:32 +0300

    Expand filter tests to include available_allocation

--- backend/restapi/tests/test_api_filter_consultant.py
+++ backend/restapi/tests/test_api_filter_consultant.py
@@ -3,6 +3,7 @@ from django.test.client import encode_multipart
 from django.urls import reverse
 from rest_framework.test import APIRequestFactory, APITestCase, APIClient
 from restapi.models import Employees, Techs, Employee_tech_skills, Certificate, Project, Employee_certificates, Employee_projects
+import datetime
 class EmployeeFilterTests(APITestCase):
     def setUp(self):
 
@@ -47,33 +48,34 @@ class EmployeeFilterTests(APITestCase):
             vendor='Microsoft',
         )
 
-        project_1 = Project.objects.create(
+
+        upcoming_project = Project.objects.create(
             project_name='Project 1',
-            project_start_date='2020-01-01',
-            project_end_date='2020-12-31',
+            project_start_date=(datetime.date.today() + datetime.timedelta(days=10)).strftime('%Y-%m-%d'),
+            project_end_date=(datetime.date.today() + datetime.timedelta(days=90)).strftime('%Y-%m-%d') ,
             confidential=True,
         )
-        project_2 = Project.objects.create(
+        ongoing_project = Project.objects.create(
             project_name='Project 2',
-            project_start_date='2022-01-01',
-            project_end_date='2023-12-31',
+            project_start_date=(datetime.date.today() + datetime.timedelta(days=-60)).strftime('%Y-%m-%d') ,
+            project_end_date=(datetime.date.today() + datetime.timedelta(days=30)).strftime('%Y-%m-%d') ,
             confidential=False,
         )
-        project_3 = Project.objects.create(
+        past_project = Project.objects.create(
             project_name='Project 3',
-            project_start_date='2023-01-01',
-            project_end_date='2024-12-31',
+            project_start_date=(datetime.date.today() + datetime.timedelta(days=-90)).strftime('%Y-%m-%d') ,
+            project_end_date=(datetime.date.today() + datetime.timedelta(days=-30)).strftime('%Y-%m-%d') ,
             confidential=True,
         )
 
-        consultant_1 = Employees.objects.create(
+        self.consultant_1 = Employees.objects.create(
             first_name='John',
             last_name='Doe',
             email='tester@gmail.com',
             phone_number='1234567890',
             location_country='Finland',
             location_city='Helsinki',
-            worktime_allocation='80',
+            worktime_allocation='100',
             allocation_until='2021-01-01',
             wants_to_do='Backend',
             wants_not_to_do='Frontend',
@@ -81,48 +83,47 @@ class EmployeeFilterTests(APITestCase):
 
 
 
-        consultant_2 = Employees.objects.create(
+        self.consultant_2 = Employees.objects.create(
             first_name='Jane',
             last_name='Watson',
             email='watson@gmail.com',
             phone_number='0415559091',
             location_country='Finland',
             location_city='Vaasa',
-            worktime_allocation='100',
+            worktime_allocation='80',
             allocation_until='2024-01-01',
             wants_to_do='Django',
             wants_not_to_do='Cobol',
         )
 
-        consultant_3 = Employees.objects.create(
+        self.consultant_3 = Employees.objects.create(
             first_name='Pauline',
             last_name='Robson',
             email='robson@gmail.com',
             phone_number='7779995551',
             location_country='Finland',
             location_city='Espoo',
-            worktime_allocation='600',
+            worktime_allocation='40',
             allocation_until='2023-07-01',
             wants_to_do='GitHub',
             wants_not_to_do='GitLab',
         )
 
 
-        Employee_tech_skills.objects.create(employee=consultant_1, tech=tech_python, skill_level=1, tech_preference=True)
-        Employee_tech_skills.objects.create(employee=consultant_1, tech=tech_django, skill_level=2, tech_preference=True)
-        Employee_tech_skills.objects.create(employee=consultant_1, tech=tech_react, skill_level=3, tech_preference=False)
-        Employee_tech_skills.objects.create(employee=consultant_2, tech=tech_js, skill_level=1, tech_preference=True)
-        Employee_tech_skills.objects.create(employee=consultant_2, tech=tech_django, skill_level=1, tech_preference=False)
-        Employee_certificates.objects.create(employee=consultant_1, cert=cert_aws_1, valid_until='2023-01-01')
-        Employee_certificates.objects.create(employee=consultant_1, cert=cert_aws_2, valid_until='2024-01-01')
-        Employee_certificates.objects.create(employee=consultant_1, cert=cert_ms_3, valid_until='2023-06-01')
-        Employee_certificates.objects.create(employee=consultant_2, cert=cert_ms_1, valid_until='2023-01-01')
-        Employee_certificates.objects.create(employee=consultant_2, cert=cert_ms_2, valid_until='2024-06-01')
-        Employee_projects.objects.create(employee=consultant_1, project=project_1)
-        Employee_projects.objects.create(employee=consultant_1, project=project_2)
-        Employee_projects.objects.create(employee=consultant_2, project=project_1)
-        Employee_projects.objects.create(employee=consultant_2, project=project_3)
-
+        Employee_tech_skills.objects.create(employee=self.consultant_1, tech=tech_python, skill_level=1, tech_preference=True)
+        Employee_tech_skills.objects.create(employee=self.consultant_1, tech=tech_django, skill_level=2, tech_preference=True)
+        Employee_tech_skills.objects.create(employee=self.consultant_1, tech=tech_react, skill_level=3, tech_preference=False)
+        Employee_tech_skills.objects.create(employee=self.consultant_2, tech=tech_js, skill_level=1, tech_preference=True)
+        Employee_tech_skills.objects.create(employee=self.consultant_2, tech=tech_django, skill_level=1, tech_preference=False)
+        Employee_certificates.objects.create(employee=self.consultant_1, cert=cert_aws_1, valid_until='2023-01-01')
+        Employee_certificates.objects.create(employee=self.consultant_1, cert=cert_aws_2, valid_until='2024-01-01')
+        Employee_certificates.objects.create(employee=self.consultant_1, cert=cert_ms_3, valid_until='2023-06-01')
+        Employee_certificates.objects.create(employee=self.consultant_2, cert=cert_ms_1, valid_until='2023-01-01')
+        Employee_certificates.objects.create(employee=self.consultant_2, cert=cert_ms_2, valid_until='2024-06-01')
+        self.consultant_1_ongoing_project = Employee_projects.objects.create(employee=self.consultant_1, project=ongoing_project, employee_participation_start_date=(datetime.date.today() + datetime.timedelta(days=-60)).strftime('%Y-%m-%d'), employee_participation_end_date=(datetime.date.today() + datetime.timedelta(days=30)).strftime('%Y-%m-%d'), allocation_busy=80)
+        self.consultant_1_upcoming_project = Employee_projects.objects.create(employee=self.consultant_1, project=upcoming_project, employee_participation_start_date=(datetime.date.today() + datetime.timedelta(days=10)).strftime('%Y-%m-%d'), employee_participation_end_date=(datetime.date.today() + datetime.timedelta(days=90)).strftime('%Y-%m-%d'), allocation_busy=20)
+        Employee_projects.objects.create(employee=self.consultant_2, project=past_project, employee_participation_start_date=(datetime.date.today() + datetime.timedelta(days=-90)).strftime('%Y-%m-%d'), employee_participation_end_date=(datetime.date.today() + datetime.timedelta(days=-30)).strftime('%Y-%m-%d'), allocation_busy=50)
+        Employee_projects.objects.create(employee=self.consultant_2, project=upcoming_project, employee_participation_start_date=(datetime.date.today() + datetime.timedelta(days=10)).strftime('%Y-%m-%d'), employee_participation_end_date=(datetime.date.today() + datetime.timedelta(days=90)).strftime('%Y-%m-%d'), allocation_busy=50)
     
     def test_filter_with_last_name_returns_one_match(self):
         search_url = '/api/consultant/?last_name=Watson'
@@ -207,3 +208,33 @@ class EmployeeFilterTests(APITestCase):
         response = self.client.get(search_url)
         result = response.json()
         self.assertEqual(len(result), 2)
+
+    def test_on_this_day_two_consultants_are_available_for_project_requiring_40_free_allocation(self):
+        search_url = '/api/consultant/?available_allocation=40'
+        response = self.client.get(search_url)
+        result = response.json()
+        self.assertEqual(len(result), 2)
+
+
+    def test_in_40_days_there_should_be_one_consultant_availble_for_project_requiring_45_free_allocation(self):
+        search_date = (datetime.date.today() + datetime.timedelta(days=40)).strftime("%Y-%m-%d")
+        search_url = '/api/consultant/?available_allocation=45,' + search_date
+        response = self.client.get(search_url)
+        result = response.json()
+        self.assertEqual(len(result), 1)
+
+    def test_in_100_days_three_consultants_are_available_for_project_requiring_40_free_allocation(self):
+        search_date = (datetime.date.today() + datetime.timedelta(days=100)).strftime("%Y-%m-%d")
+        search_url = '/api/consultant/?available_allocation=40,'+ search_date
+        response = self.client.get(search_url)
+        result = response.json()
+        self.assertEqual(len(result), 3)
+
+    def test_in_20_days_no_consultants_are_available_for_project_requiring_50_free_allocation(self):
+        search_date = (datetime.date.today() + datetime.timedelta(days=20)).strftime("%Y-%m-%d")
+        search_url = '/api/consultant/?available_allocation=50,'+ search_date
+        response = self.client.get(search_url)
+        result = response.json()
+        self.assertEqual(len(result), 0)
+        
+
