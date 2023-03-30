40e2ad16e1eb34b2f21c58bc5192b45424e6ee59
Jaakko <jiisala@gmail.com>
2023-03-30 15:33:35 +0300

    refactoring skillCard continues

--- frontend/src/Components/SkillsCard.js
+++ frontend/src/Components/SkillsCard.js
@@ -38,11 +38,10 @@ const SkillsCard = ({ user, activeUserId }) => {
   const newSkillAddable = useSelector((state) => state.skillCard.newSkillAddable)
   const skillChanges = useSelector((state) => state.skillCard.skillChanges) // This handles the changes in existing skills and existing preferences
   const addableSkillDetail = useSelector((state) => state.skillCard.addableSkillDetail)
-  const allSkills = useSelector((state) => state.skillCard.allSkills)// used when allSkill?.map() is used
-  const allerSkills = useSelector((state) => state.consultants.allTechSkills)
+  const userSkills = useSelector((state) => state.skillCard.userSkills)// used when userSkill?.map() is used
+  const allSkills = useSelector((state) => state.consultants.allTechSkills)
 
   const [trigger, setTrigger] = useState(false)
- // const [newSkillLevel, setNewSkillLevel] = useState(1)
 
   useEffect(() =>{
     const id = (activeUserId===user.id)? activeUserId : user.id
@@ -57,34 +56,17 @@ const SkillsCard = ({ user, activeUserId }) => {
     dispatch(updateNewSkillAddability(!edit))
   }
 
-  const handleSkillChange = (event) => {
-    const value = event.target.value
-    dispatch(setSkillChanges([...skillChanges, { tech: [event.target.name][0], skill_level: value}]))
-  }
-
   const handlePrefrenceChange = (event) => {
     const value = event.target.checked
     console.log(event.target.id)
     dispatch(setSkillChanges([...skillChanges, { tech: [event.target.name][0],  tech_preference: value }]))
   }
-  const handleTechChange = (event, value) => {
-    //const name = event.target.name
-    const skillId = value.id
-    const label = value.label
-    console.log("event", "idd", skillId, "label", label)
-
-    dispatch(setAddableSkillDetail({...addableSkillDetail, new_skill_id: skillId}))
-  }
-  // This method now handles both, adding the new skill and updating the consultant afterwards.
-  // That means that there is some reduntant repetition of code, feel free to refactor.
-
+  
   const handleNewSkill = async (event) => {
     event.preventDefault()
     const newSkillId = addableSkillDetail.new_skill_id
     const newSkillLevel = addableSkillDetail.new_skill_level
-    console.log("newSkillName", newSkillId, "newSkillLevel", newSkillLevel)
     let skillsList = null
-    //newObject = await techService.createTech(newSkillName)// new object contains the skill_name and id of the created skill 
     skillsList = {skills:[{skill_level: newSkillLevel, tech: newSkillId, tech_preference: true}]}
     consultantService.editConsultant(user.id, skillsList)
     dispatch(updateNewSkillAddability(!newSkillAddable))
@@ -105,7 +87,7 @@ const SkillsCard = ({ user, activeUserId }) => {
 
   const skills = () => {
     let t = []
-    allSkills?.map(
+    userSkills?.map(
       (skill) =>
         (t = t.concat([
           {
@@ -144,12 +126,7 @@ const SkillsCard = ({ user, activeUserId }) => {
 
         />
         <CardContent>
-          <Box
-            
-            // sx={{
-            //   "& .MuiTextField-root": { m: 1, width: "25ch" },
-            // }}
-          >
+          <Box>
             {newSkillAddable && (
                 <form onSubmit={handleNewSkill}>
                     
@@ -160,7 +137,7 @@ const SkillsCard = ({ user, activeUserId }) => {
                     disablePortal
                     required
                     id="skill-name"
-                    options={allerSkills.map((skill) => ({
+                    options={allSkills.map((skill) => ({
                       id: skill.id,
                       label: skill.tech_name,
                     }))}
@@ -229,21 +206,29 @@ const SkillsCard = ({ user, activeUserId }) => {
                         <TableCell>{skill.tech}</TableCell>
 
                         {/* Skill level */}
-                        <TableCell>            
-                          <TextField      
-                          key = {skill.id}
-                          disabled={!editable}
-                          select
-                          id={skill.id.toString()}
-                          name={skill.id.toString()}
-                          defaultValue={skill.skillLevel}
-                          variant="standard"
-                          onChange={handleSkillChange} // <- handleChange moved inside the Textfield element.
-                          >
-                            <MenuItem id= "Key1" key="key1" value="1">Wants to learn</MenuItem>
-                            <MenuItem id= "Key2" key="key2" value="2">Can work with</MenuItem>
-                            <MenuItem id= "Key3" key="key3" value="3">Proficient</MenuItem>
-                          </TextField>
+                        <TableCell>
+                          <Autocomplete
+                            key = {skill.id}
+                            disabled={!editable}
+                            label="Skill level"
+                            text="Define skill level"
+                            name="new_skill_level"
+                            disablePortal
+                            id={skill.id.toString()}
+                            defaultValue={skillLevels.find((level) => level.id === skill.skillLevel).level}
+                            options={skillLevels.map((skill) => ({id: skill.id, label: skill.level}))}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => (
+                              <TextField {...params} label={skill.level} />
+                            )}
+                            isOptionEqualToValue={(option, value) =>
+                              option.id === value.id
+                            }
+                            onChange={(event, value) => {
+                              dispatch(setSkillChanges([...skillChanges, { tech: event.target.id[0],  skill_level: value.id }]))
+                            }}
+                  />            
+                          
                         </TableCell>
                         <TableCell>
                         <Checkbox 
