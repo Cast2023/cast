900a9db43262473e21c7f914923332a95e74428c
Jaakko <jiisala@gmail.com>
2023-03-30 13:40:32 +0300

    first working version of refactor

--- frontend/src/Components/SkillsCard.js
+++ frontend/src/Components/SkillsCard.js
@@ -14,7 +14,8 @@ import {
   TableContainer,
   TableHead,
   Paper,
-  Checkbox
+  Checkbox,
+  Autocomplete
 } from "@mui/material"
 
 import EditIcon from "@mui/icons-material/Edit"
@@ -28,9 +29,7 @@ import { updateEditability,
         setAddableSkillDetail, 
         initializeSkillCard, } from "../Reducers/skillCardReducer";
 import { useEffect, useState } from "react";
-// MAJOR DRAWBACK: To see the changes a refresh of the page is needed. This needs to be fixed, but can be pushed to the nex sprint
-// TODO next: 
-//    Testing (robot atleast). 
+
 
 
 const SkillsCard = ({ user, activeUserId }) => {
@@ -40,8 +39,11 @@ const SkillsCard = ({ user, activeUserId }) => {
   const skillChanges = useSelector((state) => state.skillCard.skillChanges) // This handles the changes in existing skills and existing preferences
   const addableSkillDetail = useSelector((state) => state.skillCard.addableSkillDetail)
   const allSkills = useSelector((state) => state.skillCard.allSkills)// used when allSkill?.map() is used
+  const allerSkills = useSelector((state) => state.consultants.allTechSkills)
+
   const [trigger, setTrigger] = useState(false)
-  
+ // const [newSkillLevel, setNewSkillLevel] = useState(1)
+
   useEffect(() =>{
     const id = (activeUserId===user.id)? activeUserId : user.id
     dispatch(initializeSkillCard(id))// fetch consultant from database and initialize/update skills
@@ -65,26 +67,30 @@ const SkillsCard = ({ user, activeUserId }) => {
     console.log(event.target.id)
     dispatch(setSkillChanges([...skillChanges, { tech: [event.target.name][0],  tech_preference: value }]))
   }
-  const handleTechChange = (event) => {
-    const value = event.target.value
-    dispatch(setAddableSkillDetail({...addableSkillDetail, [event.target.name]: value}))
+  const handleTechChange = (event, value) => {
+    //const name = event.target.name
+    const skillId = value.id
+    const label = value.label
+    console.log("event", "idd", skillId, "label", label)
+
+    dispatch(setAddableSkillDetail({...addableSkillDetail, new_skill_id: skillId}))
   }
   // This method now handles both, adding the new skill and updating the consultant afterwards.
   // That means that there is some reduntant repetition of code, feel free to refactor.
 
   const handleNewSkill = async (event) => {
     event.preventDefault()
-    const newSkillName = {tech_name: addableSkillDetail.new_skill_name}
+    const newSkillId = addableSkillDetail.new_skill_id
     const newSkillLevel = addableSkillDetail.new_skill_level
-    let newObject = null
+    console.log("newSkillName", newSkillId, "newSkillLevel", newSkillLevel)
     let skillsList = null
-    newObject = await techService.createTech(newSkillName)// new object contains the skill_name and id of the created skill 
-    skillsList = {skills:[{skill_level: newSkillLevel, tech: newObject.result.id, tech_preference: true}]}
+    //newObject = await techService.createTech(newSkillName)// new object contains the skill_name and id of the created skill 
+    skillsList = {skills:[{skill_level: newSkillLevel, tech: newSkillId, tech_preference: true}]}
     consultantService.editConsultant(user.id, skillsList)
     dispatch(updateNewSkillAddability(!newSkillAddable))
     dispatch(setAddableSkillDetail())// This empties the state after it is not needed anymore
     //update skillChanges
-    const newSkillChanges = [...skillChanges, { skill_level: newSkillLevel, tech: newObject.result.id }]
+    const newSkillChanges = [...skillChanges, { skill_level: newSkillLevel, tech: newSkillId }]
     dispatch(setSkillChanges(newSkillChanges))
     setTrigger(!trigger)
   }
@@ -112,9 +118,8 @@ const SkillsCard = ({ user, activeUserId }) => {
     )
     return t
   }
-
-  // The free text fields are now refactored to dropdowns. Works better, but propably broke robot tests, because they expect inputs.
-  // No need to test wrong inputs anymore, so those tests can just go (robotests live in tests folder found in the root)
+  const skillLevels = [{ id: 1, level: "Beginner" }, { id: 2, level: "Intermediate" }, { id: 3, level: "Advanced" }]
+  
   return (
     <div>
       <Card>
@@ -147,32 +152,59 @@ const SkillsCard = ({ user, activeUserId }) => {
           >
             {newSkillAddable && (
                 <form onSubmit={handleNewSkill}>
-                  <div><TextField
-                      sx= {{ m: 1, width: "25ch" }}
-                      required
-                      id="skill-name"
-                      label="Add skill"
-                      variant="standard"
-                      name="new_skill_name"
-                      onChange={handleTechChange} // <- handleSkillChange moved inside the Textfield element.
-                    />
-                    </div>
-                    <div>
-                      <TextField
-                        sx= {{ m: 1, width: "25ch" }}
-                        required
-                        id="skill-level"
-                        label="Add skill level"
-                        variant="standard"
-                        name="new_skill_level"
-                        select
-                        defaultValue=""
-                        onChange={handleTechChange} // <- handleChange moved inside the Textfield element.
-                      >
-                        <MenuItem id= "Key1" key="key1" value="1">Wants to learn</MenuItem>
-                        <MenuItem id= "Key2" key="key2" value="2">Can work with</MenuItem>
-                        <MenuItem id= "Key3" key="key3" value="3">Proficient</MenuItem>
-                      </TextField></div>
+                    
+                      <Autocomplete
+                    label="Add skill"
+                    text="Add new skill"
+                    name="new_skill_name"
+                    disablePortal
+                    required
+                    id="skill-name"
+                    options={allerSkills.map((skill) => ({
+                      id: skill.id,
+                      label: skill.tech_name,
+                    }))}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Select skill" />
+                    )}
+                    isOptionEqualToValue={(option, value) =>
+                      option.id === value.id
+                    }
+                    onChange={(event, value) => {
+                      console.log("autoevent", event)
+                      console.log("autovalue", value)
+                      //handleTechChange(event, value)
+                      dispatch(setAddableSkillDetail({...addableSkillDetail, new_skill_id: value.id}))
+  
+                      //setNewProject(newValue.label)
+                      //setNewProjectId(newValue.id)
+                    }}
+                  />
+                    
+                      <Autocomplete
+                    label="Skill level"
+                    text="Define skill level"
+                    name="new_skill_level"
+                    disablePortal
+                    required={true}
+                    id="skill-level"
+                    options={skillLevels.map((skill) => ({id: skill.id, label: skill.level}))}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => (
+                      <TextField {...params} label="Select skill level" />
+                    )}
+                    isOptionEqualToValue={(option, value) =>
+                      option.id === value.id
+                    }
+                    onChange={(event, value) => {
+                      console.log("autoevent", event)
+                      console.log("autovalue", value)
+                      dispatch(setAddableSkillDetail({...addableSkillDetail, new_skill_level: value.id}))
+
+                    }}
+                  />
+                     
                     
                     <div><Button type="submit" id="submit_new_skill_button">
                       Add
