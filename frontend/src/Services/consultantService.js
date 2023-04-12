aa9438560703bd50f623720b197182a330f4f9d8
Heidi Holappa <heidi.holappa@helsinki.fi>
2023-04-12 11:08:28 +0300

    Fix issue with auth token

--- frontend/src/Services/consultantService.js
+++ frontend/src/Services/consultantService.js
@@ -4,13 +4,14 @@ const baseUrl = process.env.REACT_APP_BACKEND_URL + "api/consultant/"
 const authHeader = (token) => ({
   headers: {
     "Content-Type": "application/json",
-    Authorization: "Token token=" + token,//JSON.stringify(token)//
+    Authorization: "Token token=" + token, //JSON.stringify(token)//
     //if every user has same rights, then the user_id is not needed.
     //different user_id's different right to access different data is not handled in backend yet.
   },
 })
 
-const getAllConsultants = (APIToken) => {
+const getAllConsultants = () => {
+  const APIToken = localStorage.getItem("APIToken")
   const request = axios.get(baseUrl, authHeader(APIToken))
   return request.then((response) => response.data)
 }
